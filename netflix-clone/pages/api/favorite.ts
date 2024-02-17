c8bbbef3dff71614efeb7395161ee4c237143763
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2024-02-17 18:08:43 +0300

    imp:favoriye ekleme ve favoriden silme işlemleri yapıldı

---
+++ netflix-clone/pages/api/favorite.ts
@@ -0,0 +1,45 @@
+import serverAuth from "@/lib/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from '@/lib/prismadb'
+import { without } from "lodash";
+
+export default async function handler (req:NextApiRequest,res:NextApiResponse){
+    try {
+        const {currentUser} = await serverAuth(req,res);
+        const {movieId} = req.body;
+        const exitingMovie = await prismadb.movie.findUnique({
+            where: {id: movieId},
+        })
+        if(!exitingMovie) throw new Error('Invalid Id');
+
+        if(req.method === 'POST'){ 
+            const user = await prismadb.user.update({
+                where:{
+                    email:currentUser.email || ''
+                },
+                data:{
+                favoriteIds:{
+                    push:movieId
+                }
+                }
+            })
+    
+            return res.status(200).json(user);
+        }
+
+     if(req.method === 'DELETE'){
+        const updateFavoriIds = without(currentUser.favoriteIds,movieId);
+        const updateUser = await prismadb.user.update({
+            where:{email:currentUser.email || ''},
+            data:{
+                favoriteIds:updateFavoriIds
+            }
+        })
+        return res.status(200).json(updateUser);
+     }
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).end()
+    }
+}
