6060da9f3bfdd3f4f0cf6824f11107073bae61f6
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2024-02-18 01:05:30 +0300

    imp:favoriList tamamlandı

--- netflix-clone/components/MovieCard.tsx
+++ netflix-clone/components/MovieCard.tsx
@@ -29,7 +29,7 @@ const MovieCard: React.FC<MovieCardProp> = ({data}) => {
             </div>
             <div className='ml-auto bg-white flex transition hover:border-neutral-300 border-2 border-white
              items-center cursor-pointer w-6 lg:w-8 h-6 lg:h-8'>
-            <ChevronDownIcon className='text-black w-4 lg:w-6 cursor-pointer text-white'></ChevronDownIcon>
+            <ChevronDownIcon className='text-black w-4 lg:w-6 cursor-pointer'></ChevronDownIcon>
             </div>
             </div>
             <p className='text-green-500 font-semibold mt-4 text-sm'>New <span className='ml-2 text-white'>
